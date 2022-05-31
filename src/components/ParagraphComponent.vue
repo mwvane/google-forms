fe308171d972a09c4d6cd7f22b6594804b109728
leo <leomwvane@hotmail.com>
2022-05-31 18:49:12 +0400

    Add ParagraphComponent.vue for paragraph type

---
+++ src/components/ParagraphComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="paragraph-container">
+    <font-awesome-icon icon="align-left"></font-awesome-icon>
+    <span class="span">Long answer</span>
+  </div>
+</template>
+
+<script>
+import ComponentLayout from "@/components/ComponentLayout";
+export default {
+  name: "ParagraphComponent",
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+.paragraph-container{
+  display: flex;
+  color: #504e4e;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+.span{
+  margin-left: 15px;
+  border-bottom: 1px dotted;
+  width: 70%;
+}
+</style>
