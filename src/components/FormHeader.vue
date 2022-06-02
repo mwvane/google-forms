fc4f85cfbf744dff5c1549baee78498b64d1d409
vladimer <gshatakishvili@gmail.com>
2022-06-03 00:13:01 +0400

    Remove unnecessary code from FormHeader.vue

---
+++ src/components/FormHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid p-4 bg-light">
+    <img class="img" width="50" src="../assets/g-forms-logo.png" alt="logo">
+    <span class="mx-3">{{title}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormHeader",
+  data() {
+    return {
+      title: "Untitled",
+      description: "",
+      question: "Untitled Question",
+      options: [],
+    }
+  }
+}
+</script>
+
+<style>
+</style>
