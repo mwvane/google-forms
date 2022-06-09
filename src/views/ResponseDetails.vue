af5b02a06b59e27939c8b9bbaacab33dbe53293d
leomw <bzishvili57@gmail.com>
2022-06-09 15:49:54 +0400

    change box-shadow style

--- src/views/ResponseDetails.vue
+++ src/views/ResponseDetails.vue
@@ -50,8 +50,6 @@ export default {
 }
 .answers{
   max-width: 70%;
-  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%),
-  0 3px 1px -2px rgb(0 0 0 / 12%),
-  0 1px 5px 0 rgb(0 0 0 / 20%);
+  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%)
 }
 </style>
